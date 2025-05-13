0b21697f1e0965a6a011401349d7b8d95f83f4b4
JonathanJairxd <jonathanjairxd02@gmail.com>
2025-05-13 15:17:11 -0500

    Taller gps J

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import {Geolocation} from '@capacitor/geolocation'
+import {
+  IonContent,
+  IonButton,
+  IonToolbar,
+  IonTitle,
+  IonHeader
+} from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonContent,
+    IonButton,
+    IonToolbar,
+    IonTitle,
+    IonHeader
+  ]
 })
 export class HomePage {
+
+  latitude: number | null = null
+  longitude: number | null = null
+
   constructor() {}
+
+  async getCurrentLocation(){
+    try{
+      const coordinates = await Geolocation.getCurrentPosition();
+      this.latitude=coordinates.coords.latitude;
+      this.longitude=coordinates.coords.longitude;
+    }catch(error){
+      console.error('No se puede obtener la ubicación', error)
+    }
+  }
 }
